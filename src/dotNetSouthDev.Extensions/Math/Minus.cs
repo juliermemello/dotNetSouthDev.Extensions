712acf31f1b97744184af17b27dfdf27b93688cc
Julierme Pereira Mello <juliermemello@live.com>
2023-09-06 14:10:51 -0300

    Update Math modules

--- src/dotNetSouthDev.Extensions/Math/Minus.cs
+++ src/dotNetSouthDev.Extensions/Math/Minus.cs
@@ -2,57 +2,21 @@
 
 public static partial class MathExtensions
 {
-    public static int Minus(this short minuend, short subtrahend)
-    {
-        int difference = minuend - subtrahend;
-        return difference;
-    }
+    public static int Minus(this short minuend, short subtrahend) => minuend - subtrahend;
 
-    public static int Minus(this short minuend, int subtrahend)
-    {
-        int difference = minuend - subtrahend;
-        return difference;
-    }
+    public static int Minus(this short minuend, int subtrahend) => minuend - subtrahend;
 
-    public static long Minus(this short minuend, long subtrahend)
-    {
-        long difference = minuend - subtrahend;
-        return difference;
-    }
+    public static long Minus(this short minuend, long subtrahend) => minuend - subtrahend;
 
-    public static int Minus(this int minuend, short subtrahend)
-    {
-        int difference = minuend - subtrahend;
-        return difference;
-    }
+    public static int Minus(this int minuend, short subtrahend) => minuend - subtrahend;
 
-    public static int Minus(this int minuend, int subtrahend)
-    {
-        int difference = minuend - subtrahend;
-        return difference;
-    }
+    public static int Minus(this int minuend, int subtrahend) => minuend - subtrahend;
 
-    public static long Minus(this int minuend, long subtrahend)
-    {
-        long difference = minuend - subtrahend;
-        return difference;
-    }
+    public static long Minus(this int minuend, long subtrahend) => minuend - subtrahend;
 
-    public static long Minus(this long minuend, short subtrahend)
-    {
-        long difference = minuend - subtrahend;
-        return difference;
-    }
+    public static long Minus(this long minuend, short subtrahend) => minuend - subtrahend;
 
-    public static long Minus(this long minuend, int subtrahend)
-    {
-        long difference = minuend - subtrahend;
-        return difference;
-    }
+    public static long Minus(this long minuend, int subtrahend) => minuend - subtrahend;
 
-    public static long Minus(this long minuend, long subtrahend)
-    {
-        long difference = minuend - subtrahend;
-        return difference;
-    }
+    public static long Minus(this long minuend, long subtrahend) => minuend - subtrahend;
 }
--- src/dotNetSouthDev.Extensions/Math/Pow.cs
+++ src/dotNetSouthDev.Extensions/Math/Pow.cs
@@ -2,9 +2,5 @@
 
 public static partial class MathExtensions
 {
-    public static double Pow(this double leftSide, double rightSide)
-    {
-        double result = System.Math.Pow(leftSide, rightSide);
-        return result;
-    }
+    public static double Pow(this double leftSide, double rightSide) => System.Math.Pow(leftSide, rightSide);
 }
